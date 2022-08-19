89b6a73b80dca3f05886a81a2184fb277cafe4cb
tannerleroy <tanner@tleroy.com>
2022-08-19 11:39:14 -0400

    updated with variables

--- main.cpp
+++ main.cpp
@@ -2,8 +2,16 @@
 // config 
 
 int main(){
-    std::cout << "Number1" << std::endl;
-    std::cout << "Number2" << std::endl;
-    std::cout << "Number3" << std::endl;
 
+    int first_number {3};
+    int second_number {7};
+    double third_number{4.50};
+
+
+    std::cout << "First number : " << first_number << std::endl;
+    std::cout << "Second number : " << second_number << std::endl;
+    std::cout << "Third number : " << third_number << std::endl;
+
+
+return 0;
 }
